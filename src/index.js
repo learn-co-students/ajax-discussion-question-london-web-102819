2f12aaa5d1c4c4d10809325300415b3577a6f01a
github username <github email address>
2019-12-11 11:15:06 +0000

    finished basics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
-
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
+  const profilePic = document.querySelector('img');
+  const email = document.querySelector("#email");
+  const dob = document.querySelector("#date_of_birth");
+  const city = document.querySelector("#city");
+  const street = document.querySelector("#street");
+  const state = document.querySelector("#state");
+  const postcode = document.querySelector("#postcode");
+  const phone = document.querySelector("#phone");
+  const cell = document.querySelector("#cell");
+  const fullname = document.getElementById("fullname");
+
+  
+  fetch('https://randomuser.me/api/')
+    .then( res => res.json() )
+    .then(displayPerson)
+
+
+  function displayPerson(json) {
+
+    const results = json.results[0];
+
+    personName = [results.name.title, results.name.first, results.name.last].join(" ");
+
+    profilePic.src = results.picture.large;
+    email.innerHTML = results.email;
+    fullname.innerHTML = personName;
+    city.innerHTML = results.location.city;
+    state.innerHTML = results.location.state;
+    postcode.innerHTML = results.location.postcode;
+    dob.innerHTML = results.dob.date.slice(0,10);
+    street.innerHTML = results.location.street.number + " " + results.location.street.name;
+    phone.innerHTML = results.phone;
+    cell.innerHTML = results.cell;
+    
+  }
 });
